8aae2f103efbff166161d6f11bfaaca84250ebd2
Ricci <juamandudz1590@gmail.com>
2024-01-21 03:57:15 +0800

    Added Header.js and header.css

---
+++ capstoneproject/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './header.css'
+
+export const Header = () => {
+  return (
+    <header>
+        <h1>Logo</h1>
+        <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+        </ul>
+        </nav>
+    </header>
+  );
+};
