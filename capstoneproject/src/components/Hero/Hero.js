d95eb609ac3ffd20ccba5cbfe2d6e28be7334adb
Ricci <juamandudz1590@gmail.com>
2024-01-24 18:53:51 +0800

    fix hero content - (minus the photo)

--- capstoneproject/src/components/Hero/Hero.js
+++ capstoneproject/src/components/Hero/Hero.js
@@ -4,15 +4,17 @@ import "./Hero.css";
 export const Hero = () => {
   return (
     <main>
-      <div className="title">Little Lemon</div>
-      <div className="subTitle">Koronadal</div>
-      <div className="description">
+      <div className="heroContent">
+        <div className="title">Little Lemon</div>
+        <div className="subTitle">Koronadal</div>
+        <div className="description">
         Discover culinary perfection at Little Lemon. Our menu showcases a
         fusion of exquisite flavors, inviting you on a journey of taste. From
         refreshing salads to savory masterpieces, every bite is a symphony of
         delight. Join us for an unforgettable dining experience.
+        </div>
+        <div className="resButton">Reserve a table</div>
       </div>
-      <div className="resButton">Reserve a table</div>
     </main>
   );
 };
